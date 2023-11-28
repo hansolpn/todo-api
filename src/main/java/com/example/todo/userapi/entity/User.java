247c3a5025c6cd908acd20f1542aed89edd4e450
hansol na <142369934+hansolpn@users.noreply.github.com>
2023-11-28 14:31:22 +0900

    231127 user api login
    #7. User 로그인 기능 구현 (JWT 발급)

--- src/main/java/com/example/todo/userapi/entity/User.java
+++ src/main/java/com/example/todo/userapi/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.todo.userapi.entity;
 
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -32,4 +33,9 @@ public class User {
 
     @CreationTimestamp
     private LocalDateTime joinDate;
+
+    @Enumerated(EnumType.STRING)
+    //@ColumnDefault("'COMMON'")
+    @Builder.Default
+    private Role role = Role.COMMON;
 }
