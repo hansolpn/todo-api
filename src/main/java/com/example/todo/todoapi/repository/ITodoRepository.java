1bc33bf111ce6b070e1b968eae8e9cc0f2ed4749
hansol na <142369934+hansolpn@users.noreply.github.com>
2023-11-29 10:48:15 +0900

    231128 user api login
    #8. JWT 토큰 인증을 활용한 할일 등록 및 목록 불러오기

--- src/main/java/com/example/todo/todoapi/repository/ITodoRepository.java
+++ src/main/java/com/example/todo/todoapi/repository/ITodoRepository.java
@@ -1,8 +1,16 @@
 package com.example.todo.todoapi.repository;
 
 import com.example.todo.todoapi.entity.Todo;
+import com.example.todo.userapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ITodoRepository
         extends JpaRepository<Todo, String> {
+    // 특정 회원의 할 일 목록 리턴
+    @Query("SELECT t FROM Todo t WHERE t.user = :user")
+    List<Todo> findAllByUser(@Param("user") User user);
 }
